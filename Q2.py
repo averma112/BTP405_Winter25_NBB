388ee62df7d33b295c2a1c881dadeffbfac268c6
Vashu#112 <ashutosh.1557@gmail.com>
2025-01-15 18:03:45 -0500

    Completed code

--- Q2.py
+++ Q2.py
@@ -1,1 +1,17 @@
-import matplotlib
+import matplotlib.pyplot as plt
+month = ["January", "February", "March", "April", "May", "June", "July",
+         "August", "Septempber", "October", "November", "December"]
+rainfall = [75, 60, 100, 125, 175, 200, 225, 210, 180, 140, 90, 80]
+
+
+
+plt.figure(figsize=(10,6))
+plt.bar(month, rainfall, color="red", edgecolor="black")
+
+plt.xlabel("Months")
+plt.ylabel("Rainfall")
+plt.title("Monthly Rainfall Distribution", fontsize=12)
+plt.xticks(rotation=45)
+plt.tight_layout()
+#Show the plot using plt.show
+plt.show()
